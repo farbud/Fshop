ef527a524da2b3a5607c7321d47a4e9d297a5127
farbod <farbudskate@gmail.com>
2025-10-08 18:55:07 +0330

    some change commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,11 @@
 "use client";
 import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
+import { useCart } from "@/app/context/CartContext";
+import { ShoppingCart } from "lucide-react";
 
 export default function Navbar() {
+  const { cart } = useCart();
   return (
     <nav className="flex flex-col sm:flex-row items-center justify-between px-6 py-4 shadow-md bg-gray-100 dark:bg-gray-800">
       <Link href="/" className="text-2xl  text-gray-100 font-bold mb-2 sm:mb-0">
@@ -14,7 +17,15 @@ export default function Navbar() {
           Home
         </Link>
         <Link href="/cart" className="hover:text-blue-500 transition">
-          Cart
+          <ShoppingCart
+            className="text-gray-700 dark:text-gray-200"
+            size={26}
+          />
+          {cart.length > 0 && (
+            <span className=" -top-10 -right-2 bg-red-500 text-white text-xs font-semibold w-5 h-5 rounded-full flex items-center justify-center">
+              {cart.length}
+            </span>
+          )}
         </Link>
         <Link href="/checkout" className="hover:text-blue-500 transition">
           Checkout
